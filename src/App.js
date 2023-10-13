5c089c1c52feef260574989959d8aef799006853
ShrutiGupta <ShrutiGupta1604@gmail.com>
2023-10-13 16:33:06 +0530

    variable and Arrow Funtion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,158 @@
  import './App.css';
 
+ //*********Org.Prog.******  
+/* 
 function App() {
+  return (
+    <div className="App">
+    </div>  
+  );
+}
+export default App;
+
+*/
+//-----------------------------------------------
+
+//***************Function call Use Arrow functions*********
+//******************* Function call With Arrow functions  Ex.************************************** 
+
+/*
+
+function App()
+{
+  const sayHello = ()=> console.log("Function call With Arrow functions")
+  sayHello();
+  //setTimeout(sayHello,2000);
+
+return (
+    <div className="App">
+    </div>  
+  );
+}
+export default App;
+
+*/
+//*****************arrow () use  with settime************** 
+
+/*
+
+function App()
+{
+  const sayHello = ()=> console.log("Function call With Arrow functions and use setTimeout()")
+ setTimeout(sayHello,2000);
+
+ return (
+    <div className="App">
+    </div>  
+  );
+}
+export default App;
+
+*/
+
+//----------------------------------------------------------
+
+//***************Function call Without Arrow functions*********
+
+/*
+
+function App()
+{
+  function sayHello()
+  {
+    console.log("Function call Without Arrow functions")
+  }
+  sayHello();
+
+  return (
+    <div className="App">
+    </div>  
+  );
+}
+export default App;
+
+*/
+//----------------------------------------------------------
+
+//************************Variables ************************ 
+//***************let Variable (We can Change)************** 
+
+/*
+
+function App()
+{
+let  FirstVariable=5
+console.log("Let First Variable="+FirstVariable)
+First Variable=6
+console.log("Let First Variable="+FirstVariable)
+
+let SecondVariable=7
+console.log("Let Second Variable="+SecondVariable)
+SecondVariable=8
+console.log("Let Second Variable="+SecondVariable)
+
+return(
+  <div className='App'></div>
+);
+}
+
+export default App;
+
+*/
+//-----------------------------------------------
+
+//*************** var  Variable (We can Change)************** 
+
+/*
+
+function App()
+{
+
+var VarFirstVariable = 15
+console.log("Var First Variable:"+VarFirstVariable)
+VarFirstVariable = 16 
+console.log("Var First Variable:"+VarFirstVariable)
+
+return(
+  <div className='App'></div>
+);
+}
+
+export default App;
+
+*/
+//-----------------------------------------------
+
+//***************  Const  Variable (We can not Change)************** 
+
+/*
+
+function App()
+{
+const ConstVariable=10
+console.log("const first Variable="+ConstVariable)
+ConstVariable=11
+console.log("Const First Variable="+ConstVariable) // thiis Line get error because of cont var. not reassing
+
+
+return(
+  <div className='App'></div>
+);
+}
+
+export default App;
+
+*/
+//-----------------------------------------------
+
+//***************1ST REACT PROGRAME************** 
+
+/*
+
+function App() {
+
+console.log("Hello first React Programme  ")
+
   return (
     <div className="App">
     </div>  
@@ -8,3 +160,6 @@ function App() {
 }
 
 export default App;
+
+*/
+//-----------------------------------------------
